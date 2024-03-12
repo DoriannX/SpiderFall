b9469523c1524fcc644556adcd944d183b892a00
DoriannX <theodor.xav@gmail.com>
2024-03-12 16:29:27 +0100

    follow camera

--- SipiderFall/Assets/Scripts/CameraFollowPlayer.cs
+++ SipiderFall/Assets/Scripts/CameraFollowPlayer.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class CameraFollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject _player;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] Vector3 offset = new Vector3(0f, 0f, -10f);
+    [SerializeField] float smoothTime = 0.25f;
+    Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate()
     {
-        
+        if (_player != null)
+        {
+            Vector3 targetPosition = _player.transform.position + offset;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+        else
+            Debug.LogError("You forget to put the player in serialize field in CameraFollowPlayer script");
     }
 }
