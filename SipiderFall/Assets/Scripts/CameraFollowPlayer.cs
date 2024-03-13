d2df16daef1b1a73891ef7c675938cf0bd88b0b0
DoriannX <theodor.xav@gmail.com>
2024-03-13 13:27:17 +0100

    tilemap

--- SipiderFall/Assets/Scripts/CameraFollowPlayer.cs
+++ SipiderFall/Assets/Scripts/CameraFollowPlayer.cs
@@ -5,16 +5,27 @@ public class CameraFollowPlayer : MonoBehaviour
 {
     [SerializeField] GameObject _player;
 
-    [SerializeField] Vector3 offset = new Vector3(0f, 0f, -10f);
-    [SerializeField] float smoothTime = 0.25f;
-    Vector3 velocity = Vector3.zero;
+    [SerializeField] Vector3 _offset = new Vector3(0f, 0f, -10f);
+    [SerializeField] float _smoothTime = 0.25f;
+    Vector3 _velocity = Vector3.zero;
+    Transform _transform;
+    Transform _playerTransform;
+
+    private void Awake()
+    {
+        _transform = transform;
+        _playerTransform = _playerTransform;
+    }
 
     void FixedUpdate()
     {
         if (_player != null)
         {
-            Vector3 targetPosition = _player.transform.position + offset;
-            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+            Vector3 targetPosition = _playerTransform.position + _offset;
+            if(Mathf.Abs(_playerTransform.position.y - _transform.position.y) > 1)
+                _transform.position = new Vector3(_playerTransform.position.x - Vector3.down.x, _playerTransform.position.y - Vector3.down.y, -10);
+            else
+                _transform.position = Vector3.SmoothDamp(_transform.position, targetPosition, ref _velocity, _smoothTime);
         }
         else
             Debug.LogError("You forget to put the player in serialize field in CameraFollowPlayer script");
---
+++ SipiderFall/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    UnityEngine.Gyroscope _gyro;
+    Rigidbody2D _rb;
+    [SerializeField] float _tiltForce = 1;
+    
+
+    private void Start()
+    {
+        _gyro = Input.gyro;
+        _gyro.enabled = true;
+        _rb = GetComponentInChildren<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _rb.AddForce(new Vector3(Input.acceleration.x * _tiltForce, 0));
+        print(Mathf.PerlinNoise(0, 0));
+    }
+}
