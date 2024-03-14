8e0dc578ffbd257ab1d425749099d46f1dfd8a8b
DoriannX <theodor.xav@gmail.com>
2024-03-14 10:56:14 +0100

    shoot

--- SipiderFall/Assets/Scripts/CameraFollowPlayer.cs
+++ SipiderFall/Assets/Scripts/CameraFollowPlayer.cs
@@ -6,7 +6,7 @@ public class CameraFollowPlayer : MonoBehaviour
     [SerializeField] GameObject _player;
 
     [SerializeField] Vector3 _offset = new Vector3(0f, 0f, -10f);
-    [SerializeField] float _smoothTime = 0.25f;
+    //[SerializeField] float _smoothTime = 0.25f;
     Vector3 _velocity = Vector3.zero;
     Transform _transform;
     Transform _playerTransform;
@@ -21,11 +21,16 @@ public class CameraFollowPlayer : MonoBehaviour
     {
         if (_player != null)
         {
-            Vector3 targetPosition = new Vector3(0, _playerTransform.position.y + _offset.y, -10);
-            if(Mathf.Abs(_playerTransform.position.y - _transform.position.y) > 1)
+
+            _transform.position = new Vector3(0, _transform.position.y, 0);
+
+            /*Vector3 targetPosition = new Vector3(0, _playerTransform.position.y + _offset.y, -10);
+            if(_playerTransform.position.y - _transform.position.y > 1)
                 _transform.position = new Vector3(0, _playerTransform.position.y - Vector3.down.y, -10);
+            else if(_playerTransform.position.y - _transform.position.y < -1)
+                _transform.position = new Vector3(0, _playerTransform.position.y + Vector3.up.y, -10);
             else
-                _transform.position = Vector3.SmoothDamp(_transform.position, targetPosition, ref _velocity, _smoothTime);
+                _transform.position = Vector3.SmoothDamp(_transform.position, targetPosition, ref _velocity, _smoothTime);*/
         }
         else
             Debug.LogError("You forget to put the player in serialize field in CameraFollowPlayer script");
--- SipiderFall/Assets/Scripts/PlayerMovement.cs
+++ SipiderFall/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,12 @@ public class PlayerMovement : MonoBehaviour
 
     UnityEngine.Gyroscope _gyro;
     Rigidbody2D _rb;
+    Transform _transform;
     [SerializeField] float _tiltForce = 1;
+    [SerializeField] float _jumpForce = 1;
+    [SerializeField] float _maxVelocity = 1;
+    [SerializeField] float _shotRange = 1;
+    float _groundRange = .1f;
     
 
     private void Start()
@@ -14,10 +19,59 @@ public class PlayerMovement : MonoBehaviour
         _gyro = Input.gyro;
         _gyro.enabled = true;
         _rb = GetComponentInChildren<Rigidbody2D>();
+        _transform = transform;
     }
 
     private void Update()
     {
         _rb.AddForce(new Vector3(Input.acceleration.x * _tiltForce, 0));
+        Debug.DrawRay(_transform.position, Vector3.down * _shotRange, Color.green);
+    }
+
+    public void Attack(InputAction.CallbackContext context)
+    {
+        //if (context.started)
+        {
+            print(context);
+            Jump();
+
+            foreach (Transform child in _transform)
+            {
+                child.gameObject.layer = 2;
+            }
+
+            RaycastHit2D hit = Physics2D.Raycast(_transform.position, Vector3.down * _groundRange, 1);
+            bool grounded = hit.collider && hit.transform.parent.TryGetComponent<Ground>(out Ground ground);
+
+            if (!grounded)
+            {
+                RaycastHit2D shotHit = Physics2D.Raycast(_transform.position, Vector3.down * _shotRange, _shotRange);
+                if (shotHit.collider && shotHit.transform.parent.TryGetComponent<Ground>(out Ground groundShot))
+                {
+                    Shot(groundShot);
+                }
+            }
+        }
+    }
+
+    private void Shot(Ground ground = null)
+    {
+        //make the shoot
+        if (ground != null)
+        {
+            ground.DestroyGround();
+        }
+        foreach (Transform child in _transform)
+        {
+            child.gameObject.layer = 3;
+        }
+    }
+
+    private void Jump()
+    {
+        if (_rb.velocity.y < 0)
+            _rb.velocity = new Vector3(_rb.velocity.x, 0);
+        if (_rb.velocity.y <= _maxVelocity)
+            _rb.AddForce(Vector3.up * _jumpForce);
     }
 }
